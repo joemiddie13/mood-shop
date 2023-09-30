822d549173a1df64ef1d37b487c413d37b61faa6
joemiddie13 <joemiddie13@gmail.com>
2023-09-29 17:56:17 -0700

    connect cart to footer

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 import data from "./data.js"
 console.log(data)
 const itemsContainer = document.querySelector("#items")
+const itemList = document.getElementById("item-list")
+const cartQty = document.getElementById("cart-qty")
+const cartTotal = document.getElementById("cart-total")
+itemList.innerHTML = '<li> Hello World</li>'
 
 // the length of our data determines how many times this loop goes around
 for (let i = 0; i < data.length; i += 1) {
@@ -56,12 +60,24 @@ function addItem(name, price) {
 // Show Items
 function showItems() {
 	const qty = getQty()
-	console.log(`You have ${qty} items in your cart`)
-	
+	//console.log(`You have ${qty} items in your cart`)
+	cartQty.innerHTML = `You have ${qty} items in your cart`
+
+	let itemStr = ''
 	for (let i = 0; i < cart.length; i += 1) {
-        console.log(`${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+        //console.log(`${cart[i].name} $${cart[i].price} x ${cart[i].qty}`)
+		// const name = cart[i].name
+		// const price = cart[i].price
+		// const qty = cart[i].qty
+		//{ name: 'Apple', price: 0.99, qty: 3}
+		const { name, price, qty } = cart[i]
+
+		itemStr += `<li> ${name} $${price} x ${qty} = ${qty * price}</li>`
     }
-	console.log(`Total in cart: $${getTotalPrice()}`)
+	itemList.innerHTML = itemStr
+
+	//console.log(`Total in cart: $${getTotalPrice()}`)
+	cartTotal.innerHTML = `Total in cart: $${getTotalPrice()}`
 }
 
 //_____________________________________________
